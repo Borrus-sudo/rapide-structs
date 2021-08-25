81b3e412e9b04db62e783eb8d9954850c710ebd5
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-08-25 15:37:20 +0530

    style: improving stuff

---
+++ packages/compiler/src/types.d.ts
@@ -0,0 +1,41 @@
+export const enum Keywords {
+  alias = "alias",
+  ignore = "ignore",
+  middleware = "middleware",
+}
+export type Node =
+  | {
+      type: "Route";
+      isFlat: Boolean;
+      value: string;
+      children?: Node[];
+      middlewares?: Node[];
+    }
+  | {
+      type: "Middleware";
+      value: string;
+    };
+export type Lexeme =
+  | {
+      type: "Punctuator";
+      value: "[" | "]" | ",";
+    }
+  | {
+      type: "Literal";
+      value: string;
+    }
+  | {
+      type: "Keyword";
+      value: Keywords;
+    };
+export interface AST {
+  ast: Node[];
+  constructNode: (node: Node, parent?: string) => void;
+  traverse: (visitor: Function) => void;
+  compileStringRoutes: (rawRoutes: string[]) => void;
+}
+export const enum Errors {
+  ModuleNotFound = "ModuleNotFound: Error module not found",
+  IndentationError = "IndentError: Indentation error.",
+}
+export type throwError = (error: Errors, dynamicPart?: string) => void;
--- packages/compiler/test/lexer.test.ts
+++ packages/compiler/test/lexer.test.ts
@@ -1,8 +1,8 @@
-import Compiler from "../src/index";
+import Compile from "../src/index";
 describe("it should return a string", () => {
   it("should throw ModuleNotFoundError ", () => {
     try {
-      Compiler("./crap");
+      Compile("./crap");
     } catch (error) {
       expect(error.message).toStrictEqual(
         `ModuleNotFound: Error module not found. Please check the input path ./crap again`
