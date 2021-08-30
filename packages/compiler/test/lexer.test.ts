1a1fd4fea88f971e4ce209aff75a8e0a317b4f9d
(TS/JS).Gandalf(he/him) <58482194+Borrus-sudo@users.noreply.github.com>
2021-08-30 10:43:19 +0530

    Update lexer.test.ts

--- packages/compiler/test/lexer.test.ts
+++ packages/compiler/test/lexer.test.ts
@@ -2,7 +2,7 @@ import Compile from "../src/index";
 describe("it should return a string", () => {
   it("should throw ModuleNotFoundError ", () => {
     try {
-      Compile("./crap","");
+      Compile("./crap");
     } catch (error) {
       expect(error.message).toStrictEqual(
         `ModuleNotFound: Error module not found. Please check the input path ./crap again`
