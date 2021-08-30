79dcaf90509752056ced5e82964609bfedc2a6dd
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-08-30 11:01:44 +0530

    feat: add propert unit test cases

--- packages/compiler/test/lexer.test.ts
+++ packages/compiler/test/lexer.test.ts
@@ -1,12 +1,141 @@
 import Compile from "../src/index";
-describe("it should return a string", () => {
+import Tokeniser from "../src/lexer/tokeniser";
+import { join } from "path";
+import { readFileSync } from "fs";
+describe("it tests the lexer and a few errors", () => {
   it("should throw ModuleNotFoundError ", () => {
     try {
-      Compile("./crap");
+      Compile("./invalidpath");
     } catch (error) {
       expect(error.message).toStrictEqual(
-        `ModuleNotFound: Error module not found. Please check the input path ./crap again`
+        `ModuleNotFound: Error module not found. Please check the input path ./invalidpath again`
       );
     }
+    Compile(join(process.cwd(), "./test/compile.txt"));
+  });
+  it("should match the snapshot", () => {
+    const ast = Tokeniser(
+      readFileSync(join(process.cwd(), "./test/compile.txt"), {
+        encoding: "utf-8",
+      })
+    );
+    expect(ast).toEqual({
+      ast: [
+        {
+          type: "Route",
+          value: "/*",
+          isFlat: false,
+          middlewares: [],
+          alias: [],
+          ignore: false,
+          children: [
+            {
+              type: "Route",
+              value: "/api",
+              isFlat: false,
+              middlewares: [],
+              alias: [],
+              ignore: false,
+              children: [
+                {
+                  type: "Route",
+                  value: "/v1",
+                  isFlat: false,
+                  middlewares: ["crap"],
+                  alias: [],
+                  ignore: true,
+                  children: [
+                    {
+                      type: "Route",
+                      value: "/cards",
+                      isFlat: true,
+                      middlewares: ["card", "card2"],
+                      alias: ["playToo"],
+                      ignore: false,
+                    },
+                    {
+                      type: "Route",
+                      value: "/fetchInfo",
+                      isFlat: true,
+                      middlewares: [],
+                      alias: [],
+                      ignore: false,
+                    },
+                    {
+                      type: "Route",
+                      value: "/countDown",
+                      isFlat: true,
+                      middlewares: [],
+                      alias: [],
+                      ignore: false,
+                    },
+                  ],
+                },
+                {
+                  type: "Route",
+                  value: "/v2",
+                  isFlat: false,
+                  middlewares: [],
+                  alias: [],
+                  ignore: true,
+                  children: [
+                    {
+                      type: "Route",
+                      value: "/lodash",
+                      isFlat: true,
+                      middlewares: [],
+                      alias: [],
+                      ignore: false,
+                    },
+                    {
+                      type: "Route",
+                      value: "/vuejs",
+                      isFlat: true,
+                      middlewares: [],
+                      alias: [],
+                      ignore: false,
+                    },
+                    {
+                      type: "Route",
+                      value: "/reactjs",
+                      isFlat: true,
+                      middlewares: [],
+                      alias: [],
+                      ignore: false,
+                    },
+                    {
+                      type: "Route",
+                      value: "/play",
+                      isFlat: true,
+                      middlewares: [],
+                      alias: [],
+                      ignore: false,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: "Route",
+          value: "/login",
+          isFlat: false,
+          middlewares: [],
+          alias: [],
+          ignore: false,
+          children: [
+            {
+              type: "Route",
+              value: "/playThis",
+              isFlat: true,
+              middlewares: ["rate-limit"],
+              alias: ["taylorswift"],
+              ignore: false,
+            },
+          ],
+        },
+      ],
+    });
   });
 });
